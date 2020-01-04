63448a0b2fe5a6f5b259c1d2746e81c98a418281
Neer <eNeer@conaeromfg.com>
2020-01-03 16:00:16 -0800

    add list of test events for checking against factory output

--- tests/test_planning_objects_intergration.py
+++ tests/test_planning_objects_intergration.py
@@ -1,12 +1,13 @@
-from queue import PriorityQueue
-from unittest.mock import Mock
-from shop_forecasting.prioritizers import FifoPrioritizer
+from unittest import TestCase
+
 from shop_forecasting.planning_objects import (
     Factory,
     Router,
     RouterOperation,
     WorkCenter,
 )
+from shop_forecasting.prioritizers import FifoPrioritizer
+from shop_forecasting.util import EventLogger
 
 factory = Factory()
 
@@ -83,10 +84,196 @@ router_c.operations = {
 }
 
 for router in routers:
-    router.current_operation.work_center.enqueue(router.current_operation)
+    router.current_operation.work_center.enqueue(router.current_operation)  # type: ignore
+
+ts_key = "timestamp"
+ev_key = "event"
+obj_key = "planning_object"
+expected_events = [
+    # ops for all 3 routers initially queued at inventory circumventing
+    # the factory, so the initial queueing event is not recorded.
+    # since inventory has 3 slots, it may work them concurrently
+    {
+        ts_key: 0,
+        ev_key: EventLogger.OPERATION_STARTED,
+        obj_key: router_a.operations[10],
+    },
+    {
+        ts_key: 0,
+        ev_key: EventLogger.OPERATION_STARTED,
+        obj_key: router_b.operations[10],
+    },
+    {
+        ts_key: 0,
+        ev_key: EventLogger.OPERATION_STARTED,
+        obj_key: router_c.operations[10],
+    },
+    # all inventory ops here take 0.5 hrs, and since they are worked concurrently
+    # they also finish concurrently
+    {
+        ts_key: 0.5,
+        ev_key: EventLogger.OPERATION_COMPLETED,
+        obj_key: router_a.operations[10],
+    },
+    {
+        ts_key: 0.5,
+        ev_key: EventLogger.OPERATION_COMPLETED,
+        obj_key: router_b.operations[10],
+    },
+    {
+        ts_key: 0.5,
+        ev_key: EventLogger.OPERATION_COMPLETED,
+        obj_key: router_c.operations[10],
+    },
+    # router_a's machining operation is queued and started
+    {
+        ts_key: 0.5,
+        ev_key: EventLogger.OPERATION_QUEUED,
+        obj_key: router_a.operations[20],
+    },
+    {
+        ts_key: 0.5,
+        ev_key: EventLogger.OPERATION_STARTED,
+        obj_key: router_a.operations[20],
+    },
+    # router_b's turning operation is queued and started
+    {
+        ts_key: 0.5,
+        ev_key: EventLogger.OPERATION_QUEUED,
+        obj_key: router_b.operations[20],
+    },
+    {
+        ts_key: 0.5,
+        ev_key: EventLogger.OPERATION_STARTED,
+        obj_key: router_b.operations[20],
+    },
+    # router_c's shipping operation is queued and started
+    {
+        ts_key: 0.5,
+        ev_key: EventLogger.OPERATION_QUEUED,
+        obj_key: router_c.operations[20],
+    },
+    {
+        ts_key: 0.5,
+        ev_key: EventLogger.OPERATION_STARTED,
+        obj_key: router_c.operations[20],
+    },
+    # router_c's shipping operation is only 0.5 hrs, so it will be the next event
+    # that occurs.
+    {
+        ts_key: 1,
+        ev_key: EventLogger.OPERATION_COMPLETED,
+        obj_key: router_c.operations[20],
+    },
+    # router_c only had 2 operations, so it is now complete
+    {ts_key: 1, ev_key: EventLogger.ROUTER_COMPLETED, obj_key: router_c,},
+    # router_b's turning op is next to complete, with 2.5 hours remaining at this point
+    {
+        ts_key: 3.5,
+        ev_key: EventLogger.OPERATION_COMPLETED,
+        obj_key: router_b.operations[20],
+    },
+    # router_b's turning op gets queued up at the next wc, machining.
+    {
+        ts_key: 3.5,
+        ev_key: EventLogger.OPERATION_QUEUED,
+        obj_key: router_b.operations[30],
+    },
+    # At this point, router_a's machining op is still being worked, so
+    # router_b's machining op is not yet started.
+    # router_a's machining op is next to complete.
+    {
+        ts_key: 5.5,
+        ev_key: EventLogger.OPERATION_COMPLETED,
+        obj_key: router_a.operations[20],
+    },
+    # router_a's outside processing op is queued and started
+    {
+        ts_key: 5.5,
+        ev_key: EventLogger.OPERATION_QUEUED,
+        obj_key: router_a.operations[30],
+    },
+    {
+        ts_key: 5.5,
+        ev_key: EventLogger.OPERATION_STARTED,
+        obj_key: router_a.operations[30],
+    },
+    # router_b's machining op may now start
+    {
+        ts_key: 5.5,
+        ev_key: EventLogger.OPERATION_STARTED,
+        obj_key: router_b.operations[30],
+    },
+    # router_b's machining op completes
+    {
+        ts_key: 6.5,
+        ev_key: EventLogger.OPERATION_COMPLETED,
+        obj_key: router_b.operations[30],
+    },
+    # router_b outside processing queued and started
+    {
+        ts_key: 6.5,
+        ev_key: EventLogger.OPERATION_QUEUED,
+        obj_key: router_b.operations[40],
+    },
+    {
+        ts_key: 6.5,
+        ev_key: EventLogger.OPERATION_STARTED,
+        obj_key: router_b.operations[40],
+    },
+    # router_b outside processing completes
+    {
+        ts_key: 18.5,
+        ev_key: EventLogger.OPERATION_COMPLETED,
+        obj_key: router_b.operations[40],
+    },
+    # router_b shipping operation queued and started
+    {
+        ts_key: 18.5,
+        ev_key: EventLogger.OPERATION_QUEUED,
+        obj_key: router_b.operations[50],
+    },
+    {
+        ts_key: 18.5,
+        ev_key: EventLogger.OPERATION_STARTED,
+        obj_key: router_b.operations[50],
+    },
+    # router_b shipping operation completes, completing router
+    {
+        ts_key: 19.5,
+        ev_key: EventLogger.OPERATION_COMPLETED,
+        obj_key: router_b.operations[50],
+    },
+    {ts_key: 19.5, ev_key: EventLogger.ROUTER_COMPLETED, obj_key: router_b,},
+    # router_a outside processing completes
+    {
+        ts_key: 29.5,
+        ev_key: EventLogger.OPERATION_COMPLETED,
+        obj_key: router_a.operations[30],
+    },
+    # router_a shipping operation queued and started
+    {
+        ts_key: 29.5,
+        ev_key: EventLogger.OPERATION_QUEUED,
+        obj_key: router_a.operations[40],
+    },
+    {
+        ts_key: 29.5,
+        ev_key: EventLogger.OPERATION_STARTED,
+        obj_key: router_a.operations[40],
+    },
+    # router_a shipping operation completes, completing router
+    {
+        ts_key: 30.5,
+        ev_key: EventLogger.OPERATION_COMPLETED,
+        obj_key: router_a.operations[40],
+    },
+    {ts_key: 30.5, ev_key: EventLogger.ROUTER_COMPLETED, obj_key: router_a,},
+]
 
 
-def test_factory():
+def test_factory_integration():
     while factory.complete_next():
-        # TODO: validate log of events
         pass
+    # confusingly named, checks that each list contains same items
+    TestCase().assertCountEqual(factory.logger.events, expected_events)
